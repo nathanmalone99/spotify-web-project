c50de3700999e04e719ab607c09c2209b0031858
nathanmalone99 <61420616+nathanmalone99@users.noreply.github.com>
2021-05-03 17:49:41 +0100

    Finished Project :)

--- src/assets/Config.ts
+++ src/assets/Config.ts
@@ -1,3 +1,3 @@
 export class Config {
-    public static AUTH_TOKEN = 'BQDBqFoSmAD7vgWHFHw9LLhU5-Fl_iYyoSIpA2uDJMmcigsVfASmb6YVIzFo7pGLOK5gZV7IINz_pbc-Kyd5z-m7_UkcVqEUc2TPt_dE2bf_5n8ryN4YvuYAvXD7fpgnhAgQmfH6bZankn4c_aGj';
+    public static AUTH_TOKEN = 'BQAHVp70WXFyUn77rGrAI1l0Q50CIru9gx016CE_tCRdBVUEHQSnBQiQVmF07ebTbVSrDLcCkUwUJN4rur2gz12mzAlFt-fQ-feuMHkRho-IDnaetYgGD83ukrnl4iCG13R-NUtL8UdlQrb99ysx';
 }
